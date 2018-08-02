2ff1dfeae56bd69ae73d26da99a9d63ca8ea24f3
v.makarevich@hackspace.by <vitaliy.makarevich.work@gmail.com>
2018-08-02 22:57:20 +0300

    fix ticket showing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { ResultsComponent } from './content/results/results.component'
 import { CardComponent } from './content/tickets/card/card.component'
 import { TicketComponent } from './content/ticket/ticket.component'
 import { TicketService } from './services/ticket.service'
-import { QuestionComponent } from './content/question/question.component'
+import { QuestionComponent } from './content/question/question.component';
+import { LoadingComponent } from './common/loading/loading.component'
 
 const routes: Routes = [
   {
@@ -50,6 +51,7 @@ const routes: Routes = [
     CardComponent,
     TicketComponent,
     QuestionComponent,
+    LoadingComponent,
   ],
   imports: [
     BrowserModule,
